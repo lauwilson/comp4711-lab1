13f21f19b17cd103724c823ea49977a1d80aed00
Wilson Lau <lauwilson90@gmail.com>
2016-09-09 12:20:22 -0700

    Fix bugs with variable access within Student class.

--- Student.php
+++ Student.php
@@ -24,10 +24,10 @@ class Student {
     }
 
     function toString() {
-        $result = $this->first_name . ' ' . $this.surname;
-        $result .= ' (' . $this.average() . ')\n';
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' (' . $this->average() . ")\n";
         foreach ($this->emails as $which => $what)
-            $result .= $which . ': ' . $what . '\n';
+            $result .= $which . ': ' . $what . "\n";
         $result .= "\n";
         return '<pre>'.$result.'</pre>';
     }
