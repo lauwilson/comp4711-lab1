929c9f22195606f58eaeafa5a92f3ef4b0d62fa6
Wilson Lau <lauwilson90@gmail.com>
2016-09-09 12:24:14 -0700

    Sort Students before listing.

--- index.php
+++ index.php
@@ -29,6 +29,8 @@
         $second->add_grade(50);
         $students['a456'] = $second;
 
+        ksort($students);
+
         foreach ($students as $student)
             echo $student->toString();
         ?>
