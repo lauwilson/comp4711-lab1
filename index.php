9274716107ce2d40adc66d40b9b21bc5ce1bfbf0
Wilson Lau <lauwilson90@gmail.com>
2016-09-09 12:02:37 -0700

    Add index.php file.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>COMP 4711 - Lab 1</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello out there.";
+        ?>
+    </body>
+</html>
